920921ea0ad016541bb260c19fe80b7346ed379a
Nam Nguyen <nnguy072@ucr.edu>
2017-12-05 23:39:24 +0000

    implemented cross validation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random #for now
-from scipy.spatial import distance
+from scipy.spatial import distance  #for euclidian distance
 from pprint import pprint
 
 def intersect(dataSet, k):
@@ -11,16 +10,60 @@ def intersect(dataSet, k):
   return False
 
 def leave_one_out_cross_validation(dataSet, current_set_of_features, k):
-  column = [] #column in dataSet
+  column_array = []
+  #if current set isn't empty then add each corresponding column
+  #onto column_array
+  if current_set_of_features:
+    for i in range(len(current_set_of_features)):
+      column_temp = []
+      for j in range(len(dataSet)):
+        column_temp.append(float(dataSet[j][current_set_of_features[i]]))
+      column_array.append(column_temp)
+  
+  #add feature k onto column_array
+  column_temp2 = []
   for x in range(len(dataSet)):
-    column.append(dataSet[x][k])
+    column_temp2.append(float(dataSet[x][k]))
+  column_array.append(column_temp2)
   
-  for i in range(len(column)):
-    for j in range(len(column)):
-      distance.euclidian(column[i], column[j])
+  points = []
+  #form points from the row i.g. "[x,y,z,...,n]"
+  #append to points; list of point
+  for i in range(len(column_array[0])):
+    points_temp = []
+    for j in range(len(column_array)):
+      points_temp.append(float(column_array[j][i]))
+    points.append(points_temp)
   
-  return 0
+  class_list = [] #list containing if class matches or not
+  index_i = -1;
+  index_j = -1;
+  
+  #compare each points with one another
+  #find nearest neighbor for every point then check
+  #if they're the same class/if it's the correct class
+  for i in range(len(points)):
+    shortest_distance = float('inf')
+    for j in range(len(points)):
+      temp_dst = distance.euclidean(points[i],points[j])
+      if(shortest_distance > temp_dst and j != i):
+        shortest_distance = temp_dst
+        index_i = i
+        index_j = j
     
+    #if matches then true, else false1
+    if(dataSet[index_j][0] == dataSet[index_i][0]):
+      class_list.append(True)
+    else:
+      class_list.append(False)
+    
+  cnt = 0.0
+  for i in range(len(class_list)):
+    if(class_list[i]):
+      cnt += 1
+  
+  return cnt
+  
 #read in from file "data.txt" and store content into 2d list
 def populateDataSet(fileName):
   file = open(fileName, "r")    #open file
@@ -34,11 +77,11 @@ def forwardSelection(dataSet):
   current_set_of_features = [];
   best_best_accuracy = 0;
 
-  for i in range(1,len(dataSet) + 1):
+  for i in range(1,len(dataSet)):
     print "On Level " + str(i) + " of the search tree:"
     feature_to_add_on_this_level = -1
     best_accuracy_so_far = 0
-    for j in range(1, len(dataSet[0])):
+    for j in range(1,len(dataSet[i])):
       if not intersect(current_set_of_features, j):
         accuracy = leave_one_out_cross_validation(dataSet, current_set_of_features, j)
         if not current_set_of_features:
@@ -59,6 +102,8 @@ def forwardSelection(dataSet):
     if(feature_to_add_on_this_level != -1):
       print "On level " + str(i) + ", I added feature " + str(feature_to_add_on_this_level) + " to current set.\n"
       current_set_of_features.append(feature_to_add_on_this_level)
+    pprint(current_set_of_features)
+    
 
   pprint(current_set_of_features)
 
