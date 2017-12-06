23cd9990c1750756df11c2114658dd8f6bf93466
Nam Nguyen <nnguy072@ucr.edu>
2017-12-06 22:07:04 +0000

    merged?

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ def leave_one_out_cross_validation(dataSet, current_set_of_features, k, algorith
   #column_arry[[f1],[f2]...]
   #if current set isn't empty then add each corresponding column
   #onto column_array
-  
+
   index = 0
   if(algorithm == 2):
     for i, features in enumerate(current_set_of_features):
       if features == k:
         index = i
-  
+
   if current_set_of_features:
     for i in range(len(current_set_of_features)):
       column_temp = []
@@ -30,14 +30,14 @@ def leave_one_out_cross_validation(dataSet, current_set_of_features, k, algorith
         for j in range(len(dataSet)):
           column_temp.append(float(dataSet[j][current_set_of_features[i]]))
         column_array.append(column_temp)
-  
+
   if(algorithm == 1):
     #add feature k onto column_array
     column_temp2 = []
     for x in range(len(dataSet)):
       column_temp2.append(float(dataSet[x][k]))
     column_array.append(column_temp2)
-  
+
   points = []
   #form points from the row i.g. "[x,y,z,...,n]"
   #append to points; list of point
@@ -46,11 +46,11 @@ def leave_one_out_cross_validation(dataSet, current_set_of_features, k, algorith
     for j in range(len(column_array)):
       points_temp.append(float(column_array[j][i]))
     points.append(points_temp)
-  
+
   class_list = [] #list containing if class matches or not
   index_i = -1;
   index_j = -1;
-  
+
   #compare each points with one another
   #find nearest neighbor for every point then check
   #if they're the same class/if it's the correct class
@@ -62,20 +62,20 @@ def leave_one_out_cross_validation(dataSet, current_set_of_features, k, algorith
         shortest_distance = temp_dst
         index_i = i
         index_j = j
-    
+
     #if matches then true, else false1
     if(dataSet[index_j][0] == dataSet[index_i][0]):
       class_list.append(True)
     else:
       class_list.append(False)
-    
+
   cnt = 0.0
   for i in range(len(class_list)):
     if(class_list[i]):
       cnt += 1
-  
+
   return cnt
-  
+
 #read in from file "data.txt" and store content into 2d list
 def populateDataSet(fileName):
   file = open(fileName, "r")    #open file
@@ -89,7 +89,7 @@ def forwardSelection(dataSet):
   current_set_of_features = [];   #current feature list
   best_best_accuracy = 0;
   current_best_features = [];     #highest acc set of features; display at end
-  
+
   for i in range(1,len(dataSet[0])):  #traverse through number of features
     print "On Level " + str(i) + " of the search tree:"
     feature_to_add_on_this_level = -1 #feature w/ highest acc in level
@@ -105,7 +105,7 @@ def forwardSelection(dataSet):
         if(accuracy > best_accuracy_so_far):
           best_accuracy_so_far = accuracy
           feature_to_add_on_this_level = j
-    
+
     if(best_accuracy_so_far > best_best_accuracy):
       best_best_accuracy = best_accuracy_so_far
       current_best_features.append(feature_to_add_on_this_level)
@@ -114,10 +114,10 @@ def forwardSelection(dataSet):
 
     if(feature_to_add_on_this_level != -1):
       current_set_of_features.append(feature_to_add_on_this_level)
-      print "Feature set " + "".join(str(current_set_of_features)) + " was best with an accuracy of " + str(best_accuracy_so_far) + "%\n" 
+      print "Feature set " + "".join(str(current_set_of_features)) + " was best with an accuracy of " + str(best_accuracy_so_far) + "%\n"
 
   print "Best feature(s): " + "".join(str(current_best_features)) + ", with an accuracy of " + str(best_best_accuracy) + "%"
-  
+
 def backwardElimination(dataSet):
   #backwards elimination starst with a full set of features
   current_set_of_features = [];
@@ -125,7 +125,7 @@ def backwardElimination(dataSet):
     current_set_of_features.append(i)
   best_best_accuracy = 0;
   current_best_features = [];
-  
+
   for i in range(1,len(dataSet[0])-1):  #traverse number of features
     feature_to_add_on_this_level = -1
     best_accuracy_so_far = 0
@@ -135,18 +135,18 @@ def backwardElimination(dataSet):
         if intersect(current_set_of_features,j):  #choose only features still in the list to remove
           accuracy = leave_one_out_cross_validation(dataSet, current_set_of_features, j, 2)
           print "---Removing feature {" + str(j) + "}, accuracy is " + str(accuracy) + "%"
-  
+
           if(accuracy >= best_accuracy_so_far): #if accuracy increases than lvl acc
             best_accuracy_so_far = accuracy     #replaced level acc
             feature_to_add_on_this_level = j    #store index
-    
+
     if(feature_to_add_on_this_level != -1):
       index = 0
       for y, features in enumerate(current_set_of_features):
         if features == feature_to_add_on_this_level:
           index = y
       current_set_of_features.pop(index)
-      print "Feature set " + "".join(str(current_set_of_features)) + " was best with an accuracy of " + str(best_accuracy_so_far) + "%\n" 
+      print "Feature set " + "".join(str(current_set_of_features)) + " was best with an accuracy of " + str(best_accuracy_so_far) + "%\n"
 
     if(best_accuracy_so_far > best_best_accuracy):  #check if level acc is better than overall acc
       best_best_accuracy = best_accuracy_so_far     #replace overall acc
@@ -154,7 +154,7 @@ def backwardElimination(dataSet):
       #store the current set bc it's the best so far
     elif(best_accuracy_so_far < best_best_accuracy and len(current_best_features) != 1):
       print "(Warning, Accuracy has decreased! Continuing search in case of local maxima)\n"
-    
+
   print "Best features: " + "".join(str(current_best_features)) + ", with an accuracy of " + str(best_best_accuracy) + "%"
 
 def myAlgorithm():
